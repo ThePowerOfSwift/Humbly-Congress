b4f263cc895ac5de0232d9343b4afe8f472ad80f
Parankush <parankush@me.com>
2017-03-31 13:20:43 -0400

    Added calling, faxing, and event features.

---
+++ Humbly Congress/PoliticianTableViewController.swift
@@ -0,0 +1,309 @@
+//
+//  PoliticianTableViewController.swift
+//  Humbly Congress
+//
+//  Created by Parankush Bhardwaj on 3/31/17.
+//  Copyright © 2017 Parankush Bhardwaj. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import MessageUI
+
+class Politician: UITableViewCell {
+    
+    
+    @IBOutlet var portrait: UIImageView!
+    
+    
+    @IBOutlet weak var name: UILabel!
+    
+    
+    @IBOutlet var number: UILabel!
+    
+    
+    @IBAction func call(_ sender: UIButton) {
+        print("calling")
+        
+        if let url = URL(string: number.text!) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+}
+
+
+
+
+class PoliticianTableViewController: UITableViewController, CLLocationManagerDelegate {
+    
+    let messageComposer = MessageComposer()
+
+
+    @IBAction func faxPolitician(_ sender: UIButton) {
+        
+        if (messageComposer.canSendText()) {
+            // Obtain a configured MFMessageComposeViewController
+            let messageComposeVC = messageComposer.configuredMessageComposeViewController()
+            
+            // Present the configured MFMessageComposeViewController instance
+            // Note that the dismissal of the VC will be handled by the messageComposer instance,
+            // since it implements the appropriate delegate call-back
+            present(messageComposeVC, animated: true, completion: nil)
+        } else {
+            print("not allowed to fax")
+        }
+        
+    }
+    
+    
+    
+    //get your location.
+    
+    let locationManager = CLLocationManager()
+    
+    var userLocation = CLLocationManager().location!
+    
+    //put in a default random one
+    var zipcode: String = "33328"
+    
+    var nameOfThePolitician = " "
+    
+    
+    @IBOutlet var politicianTableView: UITableView!
+    
+    typealias ZipcodeCompletionBlock = (String?) -> Void
+    
+    func getTableInfo(completionBlock: @escaping ZipcodeCompletionBlock) {
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startMonitoringSignificantLocationChanges()
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: {(placemarks, error) -> Void in
+            if error != nil {
+                print("Reverse geocoder failed with error" + error!.localizedDescription)
+                completionBlock(nil)
+            } else if placemarks!.count > 0 {
+                let pm = placemarks![0]
+                self.zipcode = pm.postalCode!
+                completionBlock(self.zipcode)
+            } else {
+                print("Problem with the data received from geocoder")
+                completionBlock(nil)
+            }
+        })
+        
+    }
+    
+    
+    
+    
+    var numbers = [" "," "," "]
+    
+    var names = [" "," "," "]
+    
+    
+    
+    
+    var imageURLs = [ "https://theunitedstates.io/images/congress/original/",
+                      "https://theunitedstates.io/images/congress/original/",
+                      "https://theunitedstates.io/images/congress/original/"]
+    
+    var bioguide = [" "," "," "]
+    
+    var imageView = [UIImage(), UIImage(), UIImage()]
+    
+    var imageCounter = 0
+    
+    var counter = 0
+    
+
+
+    
+    
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.imageView[self.counter] = UIImage(data: data)!
+                self.counter += 1
+            }
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        //first, get the user's location
+        userLocation = CLLocationManager().location!
+        let latitude = "\(userLocation.coordinate.latitude)"
+        let longitude = "\(userLocation.coordinate.longitude)"
+        
+        
+        //next, use that location to create a link that uses an API to get the user's representatives.
+        var urlForJSON = "https://congress.api.sunlightfoundation.com/legislators/locate?latitude="
+        urlForJSON += latitude
+        urlForJSON += "&longitude="
+        urlForJSON += longitude
+        let url = URL(string: urlForJSON)
+        
+        //this counter is created so we can parse the data through the table cell
+        var counterCell = 0
+        
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                print("ERROR")
+            }
+            else {
+                if let content = data {
+                    do {
+                        let JSON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        if let results = JSON["results"] as? [[String: Any]] {
+                            for result in results {
+                                
+                                //read the title of the politician (senator or representative)
+                                if let title = result["title"] as? String {
+                                    self.names[counterCell] = (title)
+                                }
+                                //read the first name of the politician
+                                if let firstName = result["first_name"] as? String {
+                                    self.names[counterCell] += " "
+                                    self.names[counterCell] += firstName
+                                }
+                                //read the second name of the policitian
+                                if let lastName = result["last_name"] as? String {
+                                    self.names[counterCell] += " "
+                                    self.names[counterCell] += lastName
+                                }
+                                
+                                //now go to numbers
+                                if let number = result["phone"] as? String {
+                                    var numberWithpreFix = "tel://"
+                                    var realNumber = String()
+                                    realNumber = number.replacingOccurrences(of: "-", with: "")
+                                    numberWithpreFix = numberWithpreFix + realNumber
+                                    self.numbers[counterCell] = numberWithpreFix
+                                }
+                                
+                                //now go to bioID (for images)
+                                if let id = result["bioguide_id"] as? String {
+                                    self.bioguide[counterCell] = id
+                                    self.imageURLs[counterCell] += "\(self.bioguide[counterCell]).jpg"
+                                    let urlA = URL(string: self.imageURLs[counterCell])
+                                    self.downloadImage(url: urlA!)
+                                    self.imageCounter += 1
+                                    
+                                }
+                                counterCell += 1
+                            }
+                        }
+                    }
+                    catch {
+                        
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+
+    
+    
+    
+    //below, we load data table
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    
+    var hellocounter = 0;
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        print("bye")
+        
+         
+         if hellocounter < 500 {
+         DispatchQueue.main.async {
+         self.tableView.reloadData()
+         }
+         hellocounter += 1;
+         }
+         
+        
+        print("hello")
+        
+        //self.tableView.reloadData()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Politician", for: indexPath)
+        
+        
+        //names = names.sorted { $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedDescending }
+        
+        //below, each new variable grabs a specfic index of its value.
+        let nameOfPolitician = self.names[indexPath.row]
+        let numberToCall = self.numbers[indexPath.row]
+        
+        let imageOfPolitician = self.imageView[indexPath.row]
+        
+        if let politicianCell = cell as? Politician {
+            
+            //below, we set the 3 attribetus of each cell (name of politician, number, and their image)
+            politicianCell.name?.text = nameOfPolitician
+            
+            //politicianCell.imageOfPolitician.image = UIImage(named: imageOfPolitician)
+            politicianCell.portrait.image = imageOfPolitician
+            
+            politicianCell.number?.text = numberToCall
+            
+            //below, we are making the image circular
+            politicianCell.portrait.layer.borderWidth = 1
+            politicianCell.portrait.layer.masksToBounds = false
+            politicianCell.portrait.layer.borderColor = UIColor.white.cgColor
+            politicianCell.portrait.layer.cornerRadius = politicianCell.portrait.frame.width/2
+            politicianCell.portrait.clipsToBounds = true
+            
+        }
+        
+        return cell
+        
+        
+    }
+
+    
+    
+}
+
+
+
+
+
+
---
+++ Humbly Congress/eventChecker.swift
@@ -0,0 +1,423 @@
+//
+//  eventChecker.swift
+//  Humbly Congress
+//
+//  Created by Parankush Bhardwaj on 3/31/17.
+//  Copyright © 2017 Parankush Bhardwaj. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class eventChecker: FirstEventCell {
+    
+    var postalCode: Int = 00000
+    
+    
+    //these bottom six are for the event table
+    
+    var eventName = [" "," "," ", " "]
+    
+    var evntDate = [" "," "," ", " "]
+    
+    var evntDescription = [" "," "," ", " "]
+    
+    var evntLocation = [" "," "," ", " "]
+    
+    var evntURL = [" "," "," ", " "]
+    
+    var datePassed = false
+    
+    
+    
+    func checkIfEventPassed(event: String) {
+        
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat  = "EEEE, MMMM d, yyyy"
+        
+        
+        let dateOfTheEvent = event
+        
+        let todayAsDate = Date()
+        let today = formatter.string(from: todayAsDate)
+        
+        let formattedDateOfEvent = formatter.date(from: dateOfTheEvent)
+        let formattedVersionOfToday = formatter.date(from: today)
+        
+        if formattedDateOfEvent! < formattedVersionOfToday! {
+            datePassed = true
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    func getEvent(zipcode: Int, title: [String], date: [String], descriptions: [String], location: [String], url: [String]) {
+        
+        postalCode = zipcode
+        
+        eventName = title
+        
+        evntDate = date
+        
+        evntDescription = descriptions
+        
+        evntLocation = location
+        
+        evntURL = url
+        
+        
+        //order of if-states is from west to east
+        
+        
+        //Hawaii
+        if (96700 ... 96899).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            
+            //Alaska
+        else if (99500 ... 99999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Oregon
+        else if (97000 ... 97999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Washington
+        else if (98000 ... 99499).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //California
+        else if (90000 ... 96199).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Nevada
+        else if (88900 ... 89999).contains(postalCode) {
+            
+            eventName[0] = "Chair Town Hall"
+            evntDescription[0] = "Hosted by Senator Dean Heller"
+            evntLocation[0] = "2171 E William St, Carson City, NV 89701"
+            evntURL[0] = "www.apple.com"
+            evntDate[0] = "Sunday, April 2, 2017"
+            
+        }
+            //Arizona
+        else if (85000 ... 86999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            
+            //Utah
+        else if (84000 ... 84999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Idaho
+        else if (83200 ... 83999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Wyoming
+        else if (82000 ... 83199).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Colorado
+        else if (80000 ... 81999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //New Mexico
+        else if (87000 ... 88499).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Montana
+        else if (59000 ... 59999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //North Dakota
+        else if (58000 ... 58999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //South Dakota
+        else if (57000 ... 57999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Minnesota
+        else if (55000 ... 56799).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Iowa
+        else if (50000 ... 52999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Wisconson
+        else if (50000 ... 52999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Nebraska
+        else if (68000 ... 69999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Kansas
+        else if (66000 ... 67999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Missouri
+        else if (63000 ... 65999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Illinois
+        else if (60000 ... 62999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Oklahoma
+        else if (73000 ... 74999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Texas
+        else if (75000 ... 79999).contains(postalCode) || (56900 ... 56999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Liousiana
+        else if (70000 ... 71599).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Arkansas
+        else if (71600 ... 72999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Michigan
+        else if (48000 ... 49999).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Indiana
+        else if (46000 ... 47999).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Ohio
+        else if (43000 ... 45999).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Kentucky
+        else if (40000 ... 42999).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Ten-I-see
+        else if (37000 ... 38599).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Mississippi
+        else if (38600 ... 39799).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Alabama
+        else if (35000 ... 36999).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Georgia
+        else if (30000 ... 31999).contains(postalCode) || (39800 ... 39999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Florida
+        else if (32000 ... 34999).contains(postalCode){
+            
+            eventName[0] = "Town Hall"
+            evntDescription[0] = "Hosted by Vern Buchanan"
+            evntLocation[0] = "4209 S Tamiami Trail # 4231, Sarasota, FL 34231"
+            evntURL[0] = " "
+            evntDate[0] = "Sunday, February 26, 2017"
+            
+            eventName[1] = "March Against Police Brutality"
+            evntDescription[1] = "Peaceful march for the countless lives lost through police shootings on innocent victims."
+            evntLocation[1] = "1565 1st St, Sarasota, FL 34236"
+            evntURL[1] = " "
+            evntDate[1] = "Tuesday, February 28, 2017"
+            
+            eventName[2] = "Town Hall"
+            evntDescription[2] = "Hosted by Senator Bill Nelson"
+            evntLocation[2] = "3000 NW 83rd St E-127, Gainesville, FL 32606"
+            evntURL[2] = " "
+            evntDate[2] = "Thursday, March 2, 2017"
+            
+            eventName[3] = "The Immigrants March"
+            evntDescription[3] = "A peaceful demonstration showing solace with American immigrants to prove that the Trump Administration's anti-immigration views are not shared by the majority of Americans."
+            evntLocation[3] = "736 Central Ave, Sarasota, FL 34236"
+            evntURL[3] = " "
+            evntDate[3] = "Wednesday, March 8, 2017"
+        }
+            //South Carolina
+        else if (29000 ... 29999).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //North Carolina
+        else if (27000 ... 28999).contains(postalCode){
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Virginia
+        else if (22000 ... 24699).contains(postalCode) || (20100 ... 20199).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //West Virginia
+        else if (24600 ... 26999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+            //Maryland
+        else if (20600 ... 21999).contains(postalCode) {
+            eventName.append(" ")
+            evntDescription.append(" ")
+            evntLocation.append(" ")
+            evntURL.append(" ")
+            evntDate.append(" ")
+        }
+    }
+    
+}
